63a7fcdf87c40257575b2fbdb41c544059fb03ea
Axcis380 <133226284+Axcis380@users.noreply.github.com>
2024-01-10 22:11:14 +0100

    Upda

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers
+Fabric script bad on the file 2-do_deploy_web_static.py that cres and
+distrarc to the web servers
 """
 
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['203.53.147.124', '203.354.524.166']
 
 
 def do_pack():
-    """generates a tgz archive"""
+    """genera a tg arch"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
@@ -24,7 +24,7 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
+    """distrib an arch to the web sers"""
     if exists(archive_path) is False:
         return False
     try:
@@ -45,7 +45,7 @@ def do_deploy(archive_path):
 
 
 def deploy():
-    """creates and distributes an archive to the web servers"""
+    """creates and distr an arc to the web servers"""
     archive_path = do_pack()
     if archive_path is None:
         return False
