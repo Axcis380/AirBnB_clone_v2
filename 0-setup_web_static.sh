d65d31de9042c27e50e5bb2f5441a706419714f2
Axcis.380 <anas.baidouri@gmail.com>
2024-01-14 09:51:59 -0800

    updt

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,20 +1,32 @@
 #!/usr/bin/env bash
-# the deployment of web_static
+# Set up web servers for the deploym of web_static
 
-sudo apt-get -y update
-
-sudo apt-get -y upgrade
-
+# Install Nginx if it's not already instal
+sudo apt-get update
 sudo apt-get -y install nginx
 
+# Create necessary directories
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+# Create a fake HTML file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create a symbolic link
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-sudo chown -hR ubuntu:ubuntu /data/
+# Give ownership to ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+# Update Nginx configuration to serve the content
+sudo sed -i "/server_name _;/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 
-sudo service nginx start
+# Restart Nginx
+sudo service nginx restart
+
+exit 0
