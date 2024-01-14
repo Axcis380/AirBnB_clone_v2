2749d9cba4d83d873d8632d498e9d6d8d09b777e
Axcis.380 <anas.baidouri@gmail.com>
2024-01-14 12:57:51 -0800

    update

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,24 +12,22 @@ env.hosts = ['52.87.155.66', '54.89.109.87']
 
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
+    """Dele ut-of-date archives.
     Args:
         number (int): The number of archives to keep.
     If number is 0 or 1, keeps only the most recent archive. If
     number is 2, keeps the most and second-most recent archives,
     etc.
     """
-    number = int(number)
+    number = 1 if int(number) == 0 else int(number)
 
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
     with lcd("versions"):
-        local_archives = sorted(os.listdir("."))
-        local_archives_to_delete = local_archives[:-number]
-        local("rm -f {}".format(" ".join(local_archives_to_delete)))
+        [local("rm ./{}".format(a)) for a in archives]
 
     with cd("/data/web_static/releases"):
-        remote_archives = run("ls -tr | grep 'web_static_'", quiet=True).split()
-        # Ensure we don't delete all archives if number exceeds available archives
-        if number < len(remote_archives):
-            remote_archives_to_delete = remote_archives[:-number]
-            sudo("rm -rf {}".format(" ".join(remote_archives_to_delete)), quiet=True)
-
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
