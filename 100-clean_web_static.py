763870d2ff5b20da12b28c41251445fb59490032
Axcis.380 <anas.baidouri@gmail.com>
2024-01-14 12:48:30 -0800

    update

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,23 +12,22 @@ env.hosts = ['52.87.155.66', '54.89.109.87']
 
 
 def do_clean(number=0):
-    """Dele out-of-date archives.
+    """Delete out-of-date archives.
     Args:
         number (int): The number of archives to keep.
     If number is 0 or 1, keeps only the most recent archive. If
     number is 2, keeps the most and second-most recent archives,
     etc.
     """
-    number = 1 if int(number) == 0 else int(number)
+    number = int(number)
 
-    # Local cleaning
     with lcd("versions"):
         local_archives = sorted(os.listdir("."))
         local_archives_to_delete = local_archives[:-number]
         local("rm -f {}".format(" ".join(local_archives_to_delete)))
 
-    # Remote cleaning
     with cd("/data/web_static/releases"):
         remote_archives = run("ls -tr | grep 'web_static_'", quiet=True).split()
         remote_archives_to_delete = remote_archives[:-number]
         sudo("rm -rf {}".format(" ".join(remote_archives_to_delete)), quiet=True)
+
